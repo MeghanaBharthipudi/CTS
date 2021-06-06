d7b3e918a111beccdf60b39016263504f71d9151
MeghanaBharthipudi <65806123+MeghanaBharthipudi@users.noreply.github.com>
2021-06-06 11:44:47 +0530

    Add files via upload

---
+++ CodeAnalysis/CodeAnalysis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Boby", "Charig", "John","Denis","Alfee","Bond" };
+
+            var NamesOfB = names.Where(x => x.StartsWith("B"));
+
+            //-----------Task 2
+            Console.WriteLine("Enter a N value to print");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            while(i<n)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            //-----------Task 3
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+            }
+            catch(Exception ex)
+            {
+            }
+        }
+    }
+}
