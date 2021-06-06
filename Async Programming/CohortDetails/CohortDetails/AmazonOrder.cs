c404eefb57c99a2dbbe8775af3bacb6a79c8d396
MeghanaBharthipudi <65806123+MeghanaBharthipudi@users.noreply.github.com>
2021-06-06 09:06:42 +0530

    Add files via upload

---
+++ Async Programming/CohortDetails/CohortDetails/AmazonOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CohortDetails
+{
+    class AmazonOrder
+    {
+        public static void OrderDetails(string Seller_name, string Product_name,int Orderquantity=1,bool Isreturnable=true)
+        {
+            Console.WriteLine("Here is the order detail –" +
+                " {0} number of {1} by {2} is ordered. It’s returnable status is {3}",Orderquantity, Product_name,Seller_name,Isreturnable);
+        }
+        public void Main(string[] args)
+        {
+            string Seller_name = Console.ReadLine();
+            string Product_name = Console.ReadLine();
+            OrderDetails(Seller_name, Product_name);
+            Console.ReadLine();
+
+           // Orderquantity – Optional with default value 1
+           //Is returnable – Optional with default value TRUE
+        }
+    }
+}
---
+++ Async Programming/async_Await_usage_1/async_Await_usage_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace async_Await_usage_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is main method\n");
+            Method1();
+            Console.ReadLine();
+        }
+        public static async  void Method1()
+        {
+            Task<string> task = Method2();
+            string str = await task;
+            Console.WriteLine("The string is " + str);
+
+        }
+        public static async Task<string> Method2()
+        {
+            string str = "Wellcome to async Programming";
+            await Task.Run(() =>
+            {
+                    Console.WriteLine("This is Method 2");
+                Thread.Sleep(2000);
+
+            });
+            
+            return str;
+        }
+    }
+}
